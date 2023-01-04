806fb2adc841d76c26d419fd2694dac8b1aeca63
Sourav Chakraborty <cy.sourav@gmail.com>
2023-01-04 19:55:56 +0530

    Added screen display

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+let number1=undefined;
+let number2=undefined;
+let ope=undefined;
+let flag=0;
+
 function add(num1,num2){
+    num1=parseInt(num1);
+    num2=parseInt(num2);
     return num1+num2;
 }
 function subtract(num1,num2){
@@ -20,3 +27,43 @@ function operate(num1,num2,operator){
     else if(operator=="/")
         return divide(num1,num2);
 }
+function buttonPressed(e){
+    let screen=document.querySelector(".screen")
+    if(`${e.target.className}`=="button"){
+        let a=`${e.target.id}`;
+        if(a>=0 && a<=9){
+            if(flag==0){
+                if(number1==undefined){
+                    number1=a;
+                }
+                else{
+                    number1=number1+a;
+                }
+                screen.textContent+=number1;
+            }
+            else{
+                if(number2==undefined){
+                    number2=a;
+                }
+                else{
+                    number2=number2+a;
+                }
+                screen.textContent+=number2;
+            }
+        }
+        else if(a=='+' || a=='-' ||a=='*' ||a=='/'){
+            flag=1;
+            ope=a;
+            screen.textContent+=ope;
+        }
+        else if(a=='='){
+            number1=operate(number1,number2,ope);
+            screen.textContent=number1;
+            ope=undefined;
+            flag=0;
+            number2=undefined;
+        }
+    }
+}
+
+window.addEventListener("click",buttonPressed)
